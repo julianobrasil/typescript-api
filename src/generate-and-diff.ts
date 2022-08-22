cfedfc64aff8af5bf20edb7f8a96cbc86c8b7498
Juliano <juliano.brasil@gmail.com>
2022-08-22 10:28:00 +0000

    write file, not tested yet

--- src/generate-and-diff.ts
+++ src/generate-and-diff.ts
@@ -3,5 +3,29 @@ import fs from 'fs';
 
 export const generateAndDiff = (i: Interface) => {
     const filePath = path.resolve(path.join(__dirname, `${i.file}.ts`))
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+    if(fs.existsSync(filePath)) {
+       const fileDiffs = findContentDiffs(filePath, i.definition);
+       if(fileDiffs) {
+        const diffFilePath = path.resolve(path.join(__dirname, 'diffs', `${i.file}.ts`))
+         fs.writeFileSync(diffFilePath,fileDiffs.oldFileContentWithDifferences, 'utf8')
+       }
+    }
+    const fileContent = fs.writeFile(filePath, i.definition, 'utf8');
+}
+
+const findContentDiffs = (filePath: string, fileContent: string) => {
+    const oldContent = fs.readFileSync(filePath, 'utf8');
+    const oldFileContentSplitted = oldContent.split(/\n/);
+    const fileContentSplitted = fileContent.split(/\n/);
+    const differences = fileContentSplitted.map((line, i) => line === oldFileContentSplitted[i] ? null : {
+        oldContent: oldFileContentSplitted[i],
+        newContent: line,
+        lineNumber: i + 1,
+    }).filter(Boolean);
+    differences.forEach((diff) => {
+        oldFileContentSplitted[diff!.lineNumber - 1] += `// <= changed to ${oldContent}`
+    })
+    return differences.length ? {
+        oldFileContentWithDifferences: oldFileContentSplitted.join('\n')
+    } : null;
 }
