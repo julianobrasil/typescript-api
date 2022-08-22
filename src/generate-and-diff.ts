6afb8299fc0b8395747dd566c5ce08a17011507e
Juliano <juliano.brasil@gmail.com>
2022-08-22 10:33:50 +0000

    fix error

--- src/generate-and-diff.ts
+++ src/generate-and-diff.ts
@@ -3,14 +3,21 @@ import fs from 'fs';
 
 export const generateAndDiff = (i: Interface) => {
     const filePath = path.resolve(path.join(__dirname, `${i.file}.ts`))
-    if(fs.existsSync(filePath)) {
-       const fileDiffs = findContentDiffs(filePath, i.definition);
-       if(fileDiffs) {
-        const diffFilePath = path.resolve(path.join(__dirname, 'diffs', `${i.file}.ts`))
-         fs.writeFileSync(diffFilePath,fileDiffs.oldFileContentWithDifferences, 'utf8')
-       }
+    if (fs.existsSync(filePath)) {
+        const fileDiffs = findContentDiffs(filePath, i.definition);
+        if (fileDiffs) {
+            const diffFilePath = path.resolve(path.join(__dirname, 'diffs', `${i.file}.ts`));
+            if(fs.existsSync(diffFilePath)) {
+                fs.unlinkSync(diffFilePath);
+            }
+            fs.writeFileSync(diffFilePath, fileDiffs.oldFileContentWithDifferences, 'utf8');
+            fs.unlinkSync(filePath);
+        }
     }
-    const fileContent = fs.writeFile(filePath, i.definition, 'utf8');
+
+    fs.writeFile(filePath, i.definition, function (err: NodeJS.ErrnoException | null) {
+        console.log(`Error while writing to ${i.file}.ts`, err);
+    });
 }
 
 const findContentDiffs = (filePath: string, fileContent: string) => {
